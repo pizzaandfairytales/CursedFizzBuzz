1bcdc894d6b2f67f4222018f827667685a912546
pizzaandfairytales <calebbernard@gmail.com>
2021-05-11 19:55:21 -0400

    Update FizzBuzz3.cs

--- FizzBuzz3.cs
+++ FizzBuzz3.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace fb
+namespace oofb
 {
     class Program
     {
@@ -20,7 +20,7 @@ namespace fb
 
           // gotta initialize it to something! the compiler has no faith.
           decimal rate = myVariable;
-          for(int x = (myVariable - myVariable); x < myList.Count; x++){
+          for(int x = (myVariable - myVariable); x < myList.Count; x += myVariable / myVariable){
             if (myList.IndexOf(x+(myVariable/myVariable)) > (myVariable-myVariable) && 
                 myList.IndexOf(x+(myVariable/myVariable))+(myVariable/myVariable) > Math.Pow(myVariable, myVariable)/Math.Sqrt(myVariable)){
               rate = ((decimal)(myList[x] * myList[myVariable-(myVariable/myVariable)])/(decimal)(Math.Pow((myVariable+myVariable+Math.Sqrt(myVariable)), myVariable)));
@@ -34,7 +34,7 @@ namespace fb
           int extra = rng.Next((int)(Math.Pow((double)rate, myVariable)), (int)(Math.Pow((double)rate, f.IndexOf(f.Distinct().Last()))));
 
           var nums = new List<int>();
-          for (int x = (myVariable - myVariable); x < (Math.Pow(myVariable + myVariable + Math.Sqrt(myVariable), Math.Sqrt(myVariable))) + extra; x++){
+          for (int x = (myVariable - myVariable); x < (Math.Pow(myVariable + myVariable + Math.Sqrt(myVariable), Math.Sqrt(myVariable))) + extra; x += myVariable / myVariable){
             value = rate * value * ((myVariable/myVariable) - value);
             if (x >= extra){
               nums.Add((int)(decimal.Round(value, myVariable)*(decimal)Math.Pow((myVariable+myVariable+Math.Sqrt(myVariable)), myVariable)));
@@ -44,7 +44,7 @@ namespace fb
           nums = nums.Select((y, i) => y == nums[i] && y > extra ?  i+ (myVariable / myVariable) % nums.Distinct().Count() : y).ToList();
 
           // let's see what we wound up with.
-          for (int x = (myVariable - myVariable); x < nums.Count; x++){
+          for (int x = (myVariable - myVariable); x < nums.Count; x += myVariable / myVariable){
             string res = "";
             if (nums[x] % (myVariable - (myVariable / myVariable)) == (myVariable - myVariable)){
               res += f.Substring(myVariable - myVariable, myVariable);
