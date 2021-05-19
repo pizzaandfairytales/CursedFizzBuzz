122016d1d7abfbc0a65cf511b7e8ec3521662dcc
pizzaandfairytales <calebbernard@gmail.com>
2021-05-19 07:57:17 -0400

    Update FizzBuzz4.rb

--- FizzBuzz4.rb
+++ FizzBuzz4.rb
@@ -1,32 +1,34 @@
 require "zlib"
 rng = Random.new(10)
 
-poem = "it's a double bonanza for the quick pistols of a twilight riot.
-on his deathbed, he saw him vs. the outlaw was blatantly arrogance. 
-a decade of pushing comestibles to the loci all of it was bound, all to be got rid of.
-he had a pallid hue, and he got into a coughing fit - he was unwell.
-'get in the bathtub! get in the bunks!'
-we are united by moguls, we die for properties.
-the numskull reimposed the itching of completed times, 
-all his burliness unmade in the purging of cadavers, his items grasped by a pirate mob.
-'the poignancy of drunks,' one of the mob did openly exclaim, 'is, we are drunk, but we are pals,
-and we launched a war and the moguls are marred. so get the icebox now, 
-i'm not hashing out the concepts at the ovum of our games.
-out of all, my mug is by far the bushier! 
-i'm not at all aching to say all the precepts, to unpack the organs of our opuses.
-if we are harmed, we are harmed. but if the decree is, we are to hang for all we did:
-in the end, we are exorcised, we are the tipsiest!'
-so he miaowed, blinking at the pushcarts of the semi- brunches the hijacking tithed - ale addled, pig plumped. 
-'diplomas are not for me. i am penning the signal of a hooligan. 
-the nobles are our longhorns!' he roars - he is a rhino, but squiggled.
-the mob is tangibly, mannishly heroic, 
-a restart to brutalize the auditoria of all us squirming for the diadem of human beauty.
-it's all loanwords, nursed in boyhoods, all the poisonings of the pram -
-all the aches a man is born in, and if he is gruff in his phrasings, it's why.
-you and i are but hobbyists in the sunset of the hoopla.
-to put it mildly, the ovum of our husbands' babble is all the blundered downturns.
-i am disabused of the neural palaver of my avuncular audience.
-the beauty of the nuclear is, it's potent, but universally hid in the burnooses of our niches,
-in lieu of the big blowup we armed."
+poem = "It's a double bonanza for the quick pistols of a twilight riot.
+On his deathbed, he saw him vs. the outlaw was blatantly arrogance. 
+A decade of pushing comestibles to the loci all of it was bound, all to be got rid of.
+He had a pallid hue, and he got into a coughing fit - he was unwell.
+'Get in the bathtub! Get in the bunks!'
+We are united by moguls, we die for properties.
+The numskull reimposed the itching of completed times, 
+All his burliness unmade in the purging of cadavers, his items grasped by a pirate mob.
+
+'The poignancy of drunks,' one of the mob did openly exclaim, 'is, we are drunk, but we are pals,
+and we launched a war and the moguls are marred. So get the icebox now, 
+I'm not hashing out the concepts at the ovum of our games.
+Out of all, my mug is by far the bushier! 
+I'm not at all aching to say all the precepts, to unpack the organs of our opuses.
+If we are harmed, we are harmed. but if the decree is, we are to hang for all we did:
+In the end, we are exorcised, we are the tipsiest!'
+So he miaowed, blinking at the pushcarts of the semi- brunches the hijacking tithed - ale addled, pig plumped. 
+'Diplomas are not for me. I am penning the signal of a hooligan. 
+The nobles are our longhorns!' he roars - he is a rhino, but squiggled.
+
+The mob is tangibly, mannishly heroic, 
+A restart to brutalize the auditoria of all of us, squirming for the diadem of human beauty.
+It's all loanwords, nursed in boyhoods, all the poisonings of the pram -
+All the aches a man is born in, and if he is gruff in his phrasings, it's why.
+You and I are but hobbyists in the sunset of the hoopla.
+To put it mildly, the ovum of our husbands' babble is all the blundered downturns.
+I am disabused of the neural palaver of my avuncular audience.
+The beauty of the nuclear is, it's potent - so it is universally for the blindest of knights.
+But in lieu of the blowup, we are all in limbo."
 
 puts Zlib::Inflate.inflate(poem.downcase.gsub(/[\W&&[^\s]]/, "").split(" ").select{ |x| x.length > 3 }.join.gsub(/\s/, "").chars.select.with_index { |_, i| i % 3 == 0 }.join.chars.map { |x| "0123456789abcdef"[("a".."z").to_a.select{ |x| "cfjkqvwxyz".chars.find_index(x) == nil }.shuffle(random: rng).find_index(x)] }.join.scan(/../).map { |x| x.hex.chr }.join)
